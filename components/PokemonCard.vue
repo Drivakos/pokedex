75f1b8d23e5b7b306b98427693cb7f4f1975ebc3
Drivakos <vdrivakos@advisable.com>
2024-01-26 01:30:45 +0200

    feat(pokemon-list): refactor component structure and introduce PokemonCard

---
+++ components/PokemonCard.vue
@@ -0,0 +1,356 @@
+<template>
+  <div class="pokemon-card-wrapper">
+    <div v-if="details" class="pokemon-id">
+      #{{ details.id }}
+    </div>
+    <a :href="`/pokemon/${pokemon.name}`">
+      <div v-if="pokemon.name" class="pokemon-card-wrapper-img">
+        <nuxt-img
+            v-if="getPokemonImage(pokemon.name)"
+            :src="getPokemonImage(pokemon.name)"
+            :alt="pokemon.name"
+            width="150"
+            height="150"
+        />
+        <nuxt-img
+            v-else
+            :src="details.sprites.front_default"
+            :alt="pokemon.name"
+            width="150"
+            height="150"
+        />
+      </div>
+      <div class="card-particle" :class="getParticleClass(pokemon.name)"></div>
+      <div class="card-title">
+        {{ pokemon.name }}
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: {
+      type: Object,
+      required: true,
+    },
+    details: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getPokemonImage(name) {
+      return `https://img.pokemondb.net/sprites/home/normal/${name}.png`;
+    },
+    getParticleClass(name) {
+      const type = this.details.types[0].type.name;
+      return `type-${type}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-card-wrapper {
+  position: relative;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  justify-items: center;
+  height: 250px;
+  width: 140px;
+  aspect-ratio: 1;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0 1px 2px 0, rgba(60, 64, 67, 0.15) 0 1px 3px 1px;
+  transition: ease all 0.3s;
+
+  .card-title {
+    text-transform: capitalize;
+    mix-blend-mode: color-burn;
+    font-size: clamp(0.8rem, 1.5vw, 1rem);
+    font-weight: 700;
+  }
+
+  a {
+    position: relative;
+    text-align: center;
+    color: #000;
+    width: 100%;
+    text-decoration: none;
+
+    &:hover {
+      .pokemon-card-wrapper-img {
+        filter: drop-shadow(2px 4px 6px black);
+      }
+    }
+  }
+
+  .pokemon-card-wrapper-img {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    filter: drop-shadow(0 10px 10px rgb(0 0 0 / 10%));
+    max-width: 100%;
+
+    img {
+      -webkit-user-drag: none;
+    }
+  }
+
+  .card-particle {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    border-radius: 60%;
+    filter: blur(60px);
+    opacity: 0.7;
+    z-index: 0;
+    transition: ease all 0.5s;
+
+    &:before,
+    &:after {
+      content: "";
+      position: absolute;
+      right: -25%;
+      top: -25%;
+      height: 90%;
+      width: 90%;
+      border-radius: 60%;
+      z-index: 0;
+      transition: ease all 0.5s;
+    }
+
+    &:after {
+      bottom: -25%;
+      left: -25%;
+      top: auto;
+      right: auto;
+    }
+  }
+
+
+  .type-fairy {
+    &:before {
+      background-color: #e69fb0;
+    }
+
+    &:after {
+      background-color: #e9e;
+    }
+  }
+
+  .type-psychic {
+    &:before {
+      background-color: #9600f4;
+    }
+
+    &:after {
+      background-color: #f59;
+    }
+  }
+
+  .type-fighting {
+    &:before {
+      background-color: #ffb46b;
+    }
+
+    &:after {
+      background-color: #b54;
+    }
+  }
+
+  .type-ground {
+    &:before {
+      background-color: #d2820e;
+    }
+
+    &:after {
+      background-color: #db5;
+    }
+  }
+
+  .type-rock {
+    &:before {
+      background-color: #d2820e;
+    }
+
+    &:after {
+      background-color: #b26d09;
+    }
+  }
+
+  .type-electric {
+    &:before {
+      background-color: #ffd100;
+    }
+
+    &:after {
+      background-color: #fc3;
+    }
+  }
+
+  .type-normal {
+    &:before {
+      background-color: #f4eeb8;
+    }
+
+    &:after {
+      background-color: #c9cc99;
+    }
+  }
+
+  .type-grass {
+    &:before {
+      background-color: #67fc34;
+    }
+
+    &:after {
+      background-color: #7c5;
+    }
+  }
+
+  .type-dragon {
+    &:before {
+      background-color: #fa6c14;
+    }
+
+    &:after {
+      background-color: #76e;
+    }
+  }
+
+  .type-fire {
+    &:before {
+      background-color: #ff2e16;
+    }
+
+    &:after {
+      background-color: #f42;
+    }
+  }
+
+  .type-water {
+    &:before {
+      background-color: #08e3ff;
+    }
+
+    &:after {
+      background-color: #39f;
+    }
+  }
+
+  .type-poison {
+    &:before {
+      background-color: #d49cfb;
+    }
+
+    &:after {
+      background-color: #7300c5;
+    }
+  }
+
+  .type-ghost {
+    &:before {
+      background-color: #d49cfb;
+    }
+
+    &:after {
+      background-color: #66b;
+    }
+  }
+
+  .type-ice {
+    &:before {
+      background-color: #00ecf4;
+    }
+
+    &:after {
+      background-color: #6cf;
+    }
+  }
+
+  .type-bug {
+    &:before {
+      background-color: #77dc55;
+    }
+
+    &:after {
+      background-color: #ab2;
+    }
+  }
+
+  .type-steel {
+    &:before {
+      background-color: #a3a3a3;
+    }
+
+    &:after {
+      background-color: #a3a3a3;
+    }
+  }
+
+  .type-dark {
+    &:before {
+      background-color: #754;
+    }
+
+    &:after {
+      background-color: #754;
+    }
+  }
+
+  .type-flying {
+    &:before {
+      background-color: #89f;
+    }
+
+    &:after {
+      background-color: #89f;
+    }
+  }
+
+
+  &:hover {
+    cursor: pointer;
+
+    .card-particle {
+      &:before,
+      &:after {
+        opacity: 0.4;
+      }
+    }
+
+    .pokemon-card-wrapper-img {
+      transform: scale(1.05);
+    }
+  }
+}
+
+@keyframes rotate360 {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.card-particle {
+  animation: rotate360 7s linear both infinite;
+  transform-origin: center;
+}
+
+.pokemon-id {
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  font-size: 1rem;
+  font-weight: 700;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,36 +4,22 @@
     <div v-if="loading">Loading...</div>
     <div v-else class="pokemon-container-wrapper">
       <div class="pokemon-wrapper">
-        <div class="pokemon-card-wrapper"
-             v-for="pokemon in pokemonEntries.results"
-
-             :key="pokemon.name"
-             v-if="pokemonEntries">
-          <a :href="`/pokemon/${pokemon.name}`">
-            <div v-if="pokemon.name" class="pokemon-card-wrapper-img">
-
-              <nuxt-img v-if="getPokemonImage(pokemon.name)"
-                        :src="getPokemonImage(pokemon.name)"
-                        :alt="pokemon.name" width="150" height="150"/>
-              <nuxt-img v-else
-                        :src="pokemon.name.sprites.front_default"
-                        :alt="pokemon.name" width="150" height="150"/>
-            </div>
-            <div class="card-particle"></div>
-            <div class="card-title">
-              {{ pokemon.name }}
-            </div>
-          </a>
-        </div>
+        <PokemonCard v-for="pokemon in pokemonEntries.results"
+                     :key="pokemon.name"
+                     :pokemon="pokemon"
+                     :details="pokemonDetails[pokemon.name]"/>
       </div>
       <div class="filters-wrapper">
-        <div class="type-icon"
-             :class="getTypeClass(type.name)"
-             v-for="type in this.pokemonTypes.results">
+        <div
+            class="type-icon"
+            :class="getTypeClass(type.name), {active: selectedType.includes(type.name)}"
+            v-for="type in pokemonTypes.results"
+            :key="type.name"
+            @click="setSelectedType(type.name)"
+        >
           {{ type.name }}
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -42,23 +28,24 @@
 export default {
   data() {
     return {
-      pokemonEntries: {results: []},
+      pokemonEntries: { results: [] },
       pokemonDetails: {},
       loading: true,
       error: null,
-      pokemonTypes: []
+      pokemonTypes: [],
+      selectedType: [],
     };
   },
-  created() {
-    this.fetchData();
-    this.getAllPokemonTypes();
+  async created() {
+    await this.fetchData();
+    await this.getAllPokemonTypes();
   },
   methods: {
     async fetchData() {
       try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=4');
         this.pokemonEntries = await response.json();
-
+        await this.fetchPokemonDetails();
       } catch (error) {
         console.error('Error fetching data:', error);
         this.error = 'Error fetching data. Please try again.';
@@ -66,18 +53,28 @@ export default {
         this.loading = false;
       }
     },
-    getPokemonImage(name) {
-      return `https://img.pokemondb.net/sprites/home/normal/${name}.png`;
+    async fetchPokemonDetails() {
+      for (const pokemon of this.pokemonEntries.results) {
+        try {
+          const response = await fetch(pokemon.url);
+          this.pokemonDetails[pokemon.name] = await response.json();
+        } catch (error) {
+          console.error(`Error fetching details for ${pokemon.name}:`, error);
+        }
+      }
     },
     async getAllPokemonTypes() {
       try {
         const response = await fetch('https://pokeapi.co/api/v2/type/');
         this.pokemonTypes = await response.json();
-        console.log('Fetched types', this.pokemonTypes);
       } catch (error) {
-        console.error('Error fetching data:', error);
         this.error = 'Error fetching data. Please try again.';
       }
+    },
+    setSelectedType(type) {
+      this.selectedType = this.selectedType.includes(type)
+          ? this.selectedType.filter((item) => item !== type)
+          : [...this.selectedType, type];
     },
     getTypeClass(type) {
       return `type-${type}`;
@@ -86,14 +83,16 @@ export default {
 };
 </script>
 
+
+
 <style scoped>
-body {
-  background-image: linear-gradient(
-      68.4deg,
-      rgba(248, 182, 204, 0.8) 0.5%,
-      rgba(192, 198, 230, 0.8) 49%,
-      rgba(225, 246, 240, 0.8) 99.8%
-  );
+
+.filters-wrapper {
+  display: grid;
+  grid-template-columns: repeat(10,1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  align-items: center;
 }
 
 .pokemon-container-wrapper {
@@ -111,282 +110,9 @@ body {
   }
 }
 
-.pokemon-card-wrapper {
-  display: grid;
-  align-items: center;
-  justify-content: center;
-  justify-items: center;
-  height: 250px;
-  width: 140px;
-  aspect-ratio: 1;
-  position: relative;
-  overflow: hidden;
-  border-radius: 20px;
-
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.02),
-  0 2px 4px rgba(0, 0, 0, 0.02),
-  0 4px 8px rgba(0, 0, 0, 0.02),
-  0 8px 16px rgba(0, 0, 0, 0.02),
-  0 16px 32px rgba(0, 0, 0, 0.02),
-  0 32px 64px rgba(0, 0, 0, 0.02);
-  transition: ease all 0.3s;
-
-  .card-title {
-    text-transform: capitalize;
-    mix-blend-mode: color-burn;
-    font-size: clamp(0.8rem, 1.5vw, 1rem);
-    font-weight: 700;
-  }
-
-  a {
-    position: relative;
-    text-align: center;
-    color: #000;
-    width: 100%;
-    text-decoration: none;
-
-    &:hover {
-      .pokemon-card-wrapper-img {
-        filter: drop-shadow(2px 4px 6px black);
-      }
-    }
-  }
-
-  .pokemon-card-wrapper-img {
-    position: relative;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
-    filter: drop-shadow(0 10px 10px rgb(0 0 0 / 10%));
-    max-width: 100%;
-
-    img {
-      -webkit-user-drag: none;
-    }
-  }
-
-  .card-particle {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    left: 0;
-    top: 0;
-
-    &:before,
-    &:after {
-      content: "";
-      position: absolute;
-      right: -25%;
-      top: -25%;
-      height: 90%;
-      width: 90%;
-      border-radius: 60%;
-      filter: blur(60px);
-      opacity: 0.2;
-      z-index: 0;
-      transition: ease all 0.5s;
-    }
-
-    &:after {
-      bottom: -25%;
-      left: -25%;
-      top: auto;
-      right: auto;
-    }
-  }
-
-  &[data-type="fairy"] {
-    .card-particle {
-      &:before {
-        background-color: #e69fb0;
-      }
-
-      &:after {
-        background-color: #ff8cd0;
-      }
-    }
-  }
-
-  &[data-type="psychic"] {
-    .card-particle {
-      &:before {
-        background-color: #9600f4;
-      }
-
-      &:after {
-        background-color: #64f4c0;
-      }
-    }
-  }
-
-  &[data-type="fighting"] {
-    .card-particle {
-      &:before {
-        background-color: #ffb46b;
-      }
-
-      &:after {
-        background-color: #bd6f22;
-      }
-    }
-  }
-
-  &[data-type="ground"],
-  &[data-type="rock"] {
-    .card-particle {
-      &:before {
-        background-color: #d2820e;
-      }
-
-      &:after {
-        background-color: #382203;
-      }
-    }
-  }
-
-  &[data-type="electric"] {
-    .card-particle {
-      &:before {
-        background-color: #ffd100;
-      }
-
-      &:after {
-        background-color: #b29200;
-      }
-    }
-  }
-
-  &[data-type="normal"] {
-    .card-particle {
-      &:before {
-        background-color: #f4eeb8;
-      }
-
-      &:after {
-        background-color: #c9cc99;
-      }
-    }
-  }
-
-  &[data-type="grass"] {
-    .card-particle {
-      &:before {
-        background-color: #67fc34;
-      }
-
-      &:after {
-        background-color: #20fca3;
-      }
-    }
-  }
-
-  &[data-type="dragon"] {
-    .card-particle {
-      &:before {
-        background-color: #fa6c14;
-      }
-
-      &:after {
-        background-color: #ffac13;
-      }
-    }
-  }
-
-  &[data-type="fire"] {
-    .card-particle {
-      &:before {
-        background-color: #ff2e16;
-      }
-
-      &:after {
-        background-color: #ff5c16;
-      }
-    }
-  }
-
-  &[data-type="water"] {
-    .card-particle {
-      &:before {
-        background-color: #08e3ff;
-      }
-
-      &:after {
-        background-color: #008aff;
-      }
-    }
-  }
-
-  &[data-type="poison"],
-  &[data-type="ghost"] {
-    .card-particle {
-      &:before {
-        background-color: #d49cfb;
-      }
-
-      &:after {
-        background-color: #40a595;
-      }
-    }
-  }
-
-  &[data-type="ice"] {
-    .card-particle {
-      &:before {
-        background-color: #00ecf4;
-      }
-
-      &:after {
-        background-color: #0270f4;
-      }
-    }
-  }
-
-  &[data-type="bug"] {
-    .card-particle {
-      &:before {
-        background-color: #77dc55;
-      }
-
-      &:after {
-        background-color: #f5ee83;
-      }
-    }
-  }
-
-  &:hover {
-    cursor: pointer;
-
-    .card-particle {
-      &:before,
-      &:after {
-        opacity: 0.4;
-      }
-    }
-
-    .pokemon-card-wrapper-img {
-      transform: scale(1.05);
-    }
-  }
-}
-
-@keyframes rotate360 {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-.card-particle {
-  animation: rotate360 7s linear both infinite;
-  transform-origin: center;
-  z-index: 0;
-  position: relative;
-}
-
 .type-icon {
-  display: inline-block;
+  box-sizing: border-box;
+  cursor: pointer;
   width: 66px;
   height: 66px;
   border-radius: 33px;
@@ -409,6 +135,10 @@ body {
   &:hover {
     opacity: .8;
   }
+}
+
+.type-icon.active {
+  box-shadow: rgb(16 16 16) 0 0 0 3px;
 }
 
 .type-fairy {
